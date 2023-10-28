b70eaef65d03ef5ff157d3701264756b1b67a5a4
Anusree <anusreechandra.327@gmail.com>
2023-10-29 01:15:11 +0530

    no changes

--- guessmaster/settings.py
+++ guessmaster/settings.py
@@ -94,15 +94,17 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+    
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
             'min_length': 4,  # Replace with your desired minimum length
+            
         }
     },
+    {
+        'NAME':'django.contrib.auth.password_validation.NumericPasswordValidator'
+    }
    
   
 ]
